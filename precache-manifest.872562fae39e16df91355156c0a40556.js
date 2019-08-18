c8f49d2bc6ed88d7b1e2d1836fe76a0b87de03ce
GG <gg.is.not.working@gmail.com>
2019-08-19 01:42:52 +0800

    init

---
+++ precache-manifest.872562fae39e16df91355156c0a40556.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac1dcd5fe18a7660d3f790de200e611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd71e3abd218b2380183",
+    "url": "/static/css/2.2238321b.chunk.css"
+  },
+  {
+    "revision": "6357860e83d196b0ad7c",
+    "url": "/static/css/main.4560b39a.chunk.css"
+  },
+  {
+    "revision": "bd71e3abd218b2380183",
+    "url": "/static/js/2.0073d252.chunk.js"
+  },
+  {
+    "revision": "6357860e83d196b0ad7c",
+    "url": "/static/js/main.59ce9b35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3aa93a7ce21349d1c2ccd7dc0e8bb126",
+    "url": "/static/media/blue.3aa93a7c.png"
+  },
+  {
+    "revision": "07d2ecf6210d0424f8ac7ed267eeb8a3",
+    "url": "/static/media/fontello.07d2ecf6.woff"
+  },
+  {
+    "revision": "58f8177efd657e258feb4aa50e368165",
+    "url": "/static/media/fontello.58f8177e.eot"
+  },
+  {
+    "revision": "b306a917719f2244949a76acb1dae41d",
+    "url": "/static/media/fontello.b306a917.svg"
+  },
+  {
+    "revision": "c6ae56b6e9dee62001107d8518764f54",
+    "url": "/static/media/fontello.c6ae56b6.woff2"
+  },
+  {
+    "revision": "e8271083ac82e1f2a1c22e47257a5e78",
+    "url": "/static/media/fontello.e8271083.ttf"
+  },
+  {
+    "revision": "cf78a1d43203e5f095ab7c9cfce6a995",
+    "url": "/static/media/image.cf78a1d4.png"
+  }
+]);
