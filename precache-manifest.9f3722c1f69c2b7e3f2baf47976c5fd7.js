5041467e535cfd52c57baeb94f20b9835ccb3733
GG <gg.is.not.working@gmail.com>
2019-08-19 23:53:24 +0800

    Updates

---
+++ precache-manifest.9f3722c1f69c2b7e3f2baf47976c5fd7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19261bfe53a274f70d0e1af4b837f94",
+    "url": "https://ggisnotworking.github.io/ornot/index.html"
+  },
+  {
+    "revision": "bd71e3abd218b2380183",
+    "url": "https://ggisnotworking.github.io/ornot/static/css/2.2238321b.chunk.css"
+  },
+  {
+    "revision": "c59c5bb52523d4cea18e",
+    "url": "https://ggisnotworking.github.io/ornot/static/css/main.c27066f0.chunk.css"
+  },
+  {
+    "revision": "bd71e3abd218b2380183",
+    "url": "https://ggisnotworking.github.io/ornot/static/js/2.0073d252.chunk.js"
+  },
+  {
+    "revision": "c59c5bb52523d4cea18e",
+    "url": "https://ggisnotworking.github.io/ornot/static/js/main.ba2ec174.chunk.js"
+  },
+  {
+    "revision": "b4cbaafc1ee7a982ce8e",
+    "url": "https://ggisnotworking.github.io/ornot/static/js/runtime~main.7aff8048.js"
+  },
+  {
+    "revision": "3aa93a7ce21349d1c2ccd7dc0e8bb126",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/blue.3aa93a7c.png"
+  },
+  {
+    "revision": "07d2ecf6210d0424f8ac7ed267eeb8a3",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/fontello.07d2ecf6.woff"
+  },
+  {
+    "revision": "58f8177efd657e258feb4aa50e368165",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/fontello.58f8177e.eot"
+  },
+  {
+    "revision": "b306a917719f2244949a76acb1dae41d",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/fontello.b306a917.svg"
+  },
+  {
+    "revision": "c6ae56b6e9dee62001107d8518764f54",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/fontello.c6ae56b6.woff2"
+  },
+  {
+    "revision": "e8271083ac82e1f2a1c22e47257a5e78",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/fontello.e8271083.ttf"
+  },
+  {
+    "revision": "cf78a1d43203e5f095ab7c9cfce6a995",
+    "url": "https://ggisnotworking.github.io/ornot/static/media/image.cf78a1d4.png"
+  }
+]);
