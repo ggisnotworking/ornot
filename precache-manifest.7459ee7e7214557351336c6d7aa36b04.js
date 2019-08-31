e5227a104346303b49a74ee8867cfe12a3984275
ggisnotworking <gg.is.not.working@gmail.com>
2019-09-01 04:33:32 +0800

    Updates

--- precache-manifest.7459ee7e7214557351336c6d7aa36b04.js
+++ precache-manifest.7459ee7e7214557351336c6d7aa36b04.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5df1bbc60cf380742c0b88212369a185",
+    "revision": "93677d665110617453c03f37510f483f",
     "url": "https://ggisnotworking.github.io/ornot/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://ggisnotworking.github.io/ornot/static/css/2.ec7000dc.chunk.css"
   },
   {
-    "revision": "f7a45835da263a1f0eb9",
+    "revision": "08290c9e741a5b401fbd",
     "url": "https://ggisnotworking.github.io/ornot/static/css/main.20240c8c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://ggisnotworking.github.io/ornot/static/js/2.1c6f4d30.chunk.js"
   },
   {
-    "revision": "f7a45835da263a1f0eb9",
-    "url": "https://ggisnotworking.github.io/ornot/static/js/main.0c7b8a38.chunk.js"
+    "revision": "08290c9e741a5b401fbd",
+    "url": "https://ggisnotworking.github.io/ornot/static/js/main.968cad6e.chunk.js"
   },
   {
     "revision": "b4cbaafc1ee7a982ce8e",
